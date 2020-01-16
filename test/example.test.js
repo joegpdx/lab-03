99588861a32a71c247641960b05f54f8768567d7
joegpdx <joegpdx@gmail.com>
2020-01-16 09:58:08 -0800

    thank you Kam and Jose

--- test/example.test.js
+++ test/example.test.js
@@ -1,17 +1,47 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import rps-function from '../rps-function.js';
+import { checkResult } from '../getRandomThrow.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function (assert) {
+test('check draw', function (assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const player = 'rock';
+    const computer = 'rock';
+    const expected = 'draw';
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const shouldBeDraw = checkResult(player, computer);
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(shouldBeDraw, expected);
+});
+
+test('check win', function (assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const player = 'rock';
+    const computer = 'scissors';
+    const expected = 'win';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const shouldBeWin = checkResult(player, computer);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(shouldBeWin, expected);
+});
+
+test('check lose', function (assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const player = 'scissors';
+    const computer = 'rock';
+    const expected = 'lose';
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const shouldBelose = checkResult(player, computer);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(shouldBelose, expected);
 });
