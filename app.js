8023fcbecb65a6f6d420530cd6827fb3b2fcf9a3
joegpdx <joegpdx@gmail.com>
2020-01-16 07:04:34 -0800

    HTMl-Layout

---
+++ app.js
@@ -0,0 +1,10 @@
+import { rpsfunction, checkResult } from '../rps-function.js';
+
+const button = document.getElementById("Submit-Button")
+
+
+
+
+button.addEventListener('click', () => {
+
+})
--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,10 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
+import rps-function from '../rps-function.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('time to test a function', function (assert) {
     //Arrange
     // Set up your parameters and expectations
 
