7b06f8edbb41c507467d1dbc9f7d2a258176f5d8
joegpdx <joegpdx@gmail.com>
2020-01-16 11:06:21 -0800

    remove console logs

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ button.addEventListener('click', () => {
     const result = checkResult(userChoice, computerChoice);
     resultImage.classList.remove('hidden');
 
-    console.log('user', userChoice);
-    console.log('computer', computerChoice);
-    console.log(result);
-
     if (computerChoice === 'rock') resultImage.src = 'https://images.homedepot-static.com/productImages/fa9fe9af-bd06-4a60-be35-384709e37a65/svn/outdoor-essentials-bagged-204952-64_1000.jpg';
     if (computerChoice === 'scissors') resultImage.src = 'https://www.ikea.com/us/en/images/products/sy-scissors__0112301_PE263788_S5.JPG?f=s';
     if (computerChoice === 'paper') resultImage.src = 'https://images.ctfassets.net/f1fikihmjtrp/29Ke1N1Qv2X0KsFOC4xyHd/6258f6d0581f8fa655485da0ec61cda3/10240-1009-1-3ww-l.jpg';
