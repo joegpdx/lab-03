969b11765fe8d367afdb286b98cdeae04e842380
joegpdx <joegpdx@gmail.com>
2020-01-17 08:51:20 -0800

    fix player/computer

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ let draw = 0;
 
 button.addEventListener('click', () => {
     const userInput = document.querySelector('input:checked');
-    const userChoice = userInput.value;
-    const computerChoice = getRandomThrow();
-    const result = checkResult(userChoice, computerChoice);
+    const player = userInput.value;
+    const computer = getRandomThrow();
+    const result = checkResult(player, computer);
     resultImage.classList.remove('hidden');
 
-    if (computerChoice === 'rock') resultImage.src = 'https://images.homedepot-static.com/productImages/fa9fe9af-bd06-4a60-be35-384709e37a65/svn/outdoor-essentials-bagged-204952-64_1000.jpg';
-    if (computerChoice === 'scissors') resultImage.src = 'https://www.ikea.com/us/en/images/products/sy-scissors__0112301_PE263788_S5.JPG?f=s';
-    if (computerChoice === 'paper') resultImage.src = 'https://images.ctfassets.net/f1fikihmjtrp/29Ke1N1Qv2X0KsFOC4xyHd/6258f6d0581f8fa655485da0ec61cda3/10240-1009-1-3ww-l.jpg';
+    if (computer === 'rock') resultImage.src = 'https://images.homedepot-static.com/productImages/fa9fe9af-bd06-4a60-be35-384709e37a65/svn/outdoor-essentials-bagged-204952-64_1000.jpg';
+    if (computer === 'scissors') resultImage.src = 'https://www.ikea.com/us/en/images/products/sy-scissors__0112301_PE263788_S5.JPG?f=s';
+    if (computer === 'paper') resultImage.src = 'https://images.ctfassets.net/f1fikihmjtrp/29Ke1N1Qv2X0KsFOC4xyHd/6258f6d0581f8fa655485da0ec61cda3/10240-1009-1-3ww-l.jpg';
 
     if (result === 'win') {
         wins++;
